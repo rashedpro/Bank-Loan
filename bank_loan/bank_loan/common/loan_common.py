a7ca1a0b7fb0408e5d24fc26302d84133710d2ff
rashedpro <rashedpro2025@gmail.com>
2023-07-31 10:10:35 +0300

    fix bug in make repayment scheduler func

--- bank_loan/bank_loan/common/loan_common.py
+++ bank_loan/bank_loan/common/loan_common.py
@@ -53,36 +53,36 @@ class CustomLoan(Loan):
             if not self.repayment_start_date:
                 frappe.throw(_("Repayment Start Date is mandatory for term loans"))
 
-                schedule_type_details = frappe.db.get_value(
-                    "Loan Type", self.loan_type, ["repayment_schedule_type", "repayment_date_on"], as_dict=1
+            schedule_type_details = frappe.db.get_value(
+                "Loan Type", self.loan_type, ["repayment_schedule_type", "repayment_date_on"], as_dict=1
+            )
+
+            self.repayment_schedule = []
+            payment_date = self.repayment_start_date
+            balance_amount = self.loan_amount
+
+            while balance_amount > 0:
+                interest_amount, principal_amount, balance_amount, total_payment = self.get_amounts(
+                    payment_date,
+                    balance_amount,
+                    schedule_type_details.repayment_schedule_type,
+                    schedule_type_details.repayment_date_on,
                 )
 
-                self.repayment_schedule = []
-                payment_date = self.repayment_start_date
-                balance_amount = self.loan_amount
+                if schedule_type_details.repayment_schedule_type == "Pro-rated calendar months":
+                    next_payment_date = get_last_day(payment_date)
+                    if schedule_type_details.repayment_date_on == "Start of the next month":
+                        next_payment_date = add_days(next_payment_date, 1)
 
-                while balance_amount > 0:
-                    interest_amount, principal_amount, balance_amount, total_payment = self.get_amounts(
-                        payment_date,
-                        balance_amount,
-                        schedule_type_details.repayment_schedule_type,
-                        schedule_type_details.repayment_date_on,
-                    )
+                    payment_date = next_payment_date
 
-                    if schedule_type_details.repayment_schedule_type == "Pro-rated calendar months":
-                        next_payment_date = get_last_day(payment_date)
-                        if schedule_type_details.repayment_date_on == "Start of the next month":
-                            next_payment_date = add_days(next_payment_date, 1)
+                self.add_repayment_schedule_row(
+                    payment_date, principal_amount, interest_amount, total_payment, balance_amount
+                )
 
-                        payment_date = next_payment_date
-
-                    self.add_repayment_schedule_row(
-                        payment_date, principal_amount, interest_amount, total_payment, balance_amount
-                    )
-
-                    if (
-                        schedule_type_details.repayment_schedule_type == "Monthly as per repayment start date"
-                        or schedule_type_details.repayment_date_on == "End of the current month"
-                    ):
-                        next_payment_date = add_single_month(payment_date)
-                        payment_date = next_payment_date
+                if (
+                    schedule_type_details.repayment_schedule_type == "Monthly as per repayment start date"
+                    or schedule_type_details.repayment_date_on == "End of the current month"
+                ):
+                    next_payment_date = add_single_month(payment_date)
+                    payment_date = next_payment_date
